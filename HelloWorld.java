a92a2f282a0f3df82a73f2cb8dca89e1f04961f9
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-09-27 21:35:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class Hello {
+
+    public static void main(String[] args) { // main - Entry point of every java program
+
+        System.out.println("Hello World"); // print the output on the output screen
+        //String: It is a collection of characters
+    }
+    // class - category
+
+}
