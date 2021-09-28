bc29205d6ccf8cbcc36533b35aa2a2528696bbd6
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-09-28 21:01:08 +0530

    Add files via upload

--- Practice.java
+++ Practice.java
@@ -1,5 +1,13 @@
 class Practice {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        System.out.println("Brain Mentors");
+        System.out.print("Brain Mentors");
         System.out.println("Brain Mentors");
     }    
+
+    public static void main() {
+        
+    }
 }
+
+
