f0fee9ca6f64f98a8e594db4c6c00256b38b08b7
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-10-04 19:01:38 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,15 @@
+class Data {
+    public static void main(String[] args) {
+        
+        //float pi = 3.14f; // 1st way
+        float pi = (float)3.14;
+        char ch = 'F';
+        boolean status = false;
+        String name = "Brain Mentors";
+
+        System.out.println(pi);
+        System.out.println(ch);
+        System.out.println(status);
+        System.out.println(name);
+    }
+}
