9578c2ec45c6874b4e60bcb78199b17f6ab9f074
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-10-29 10:08:43 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -8,33 +8,48 @@ public class Student {
 	private String phone;
 	private String course;
 	private double fees;
+	private String collegeName;
+	
+	//Default Constructor
+	Student()
+	{
+		collegeName = "SRCC";
+	}
 	
 //	r, n, p, c, f - local variables
 //	scope is with in the function
-	public void takeInput(int r, String n, String p, String c, double f)
-	{
-		//instance variable = local variable
-		rollno = r;
-		name = n;
-		phone = p;
-		course = c;
-		fees = f;
-	}
+	//public void takeInput(int r, String n, String p, String c, double f)
+//	public void takeInput(int rollno, String name, String phone, String course, double fees)
+//	{
+//		//instance variable = local variable
+////		rollno = r;
+////		name = n;
+////		phone = p;
+////		course = c;
+////		fees = f;
+//		
+//		this.rollno = rollno;
+//		this.name = name;
+//		this.phone = phone;
+//		this.course = course;
+//		this.fees = fees;
+//	}
 	
 	public void print()
 	{
-		System.out.println("Rollno: "+rollno);
+		System.out.println("Rollno: "+this.rollno); // this keyword is optional here, as it is implicitly work itself.
 		System.out.println("Name: "+name);
 		System.out.println("Phone: "+phone);
 		System.out.println("Course: "+course);
 		System.out.println("Fees: "+fees);
+		System.out.println("College Name: "+collegeName);
 	}
 
 	public static void main(String[] args) { // member function / method
 		// TODO Auto-generated method stub
 		
 		Student ram = new Student(); // ram = reference variable
-		ram.takeInput(1001, "Ram", "82748794879", "MCA", 10000.0);
+		//ram.takeInput(1001, "Ram", "82748794879", "MCA", 10000.0);
 		ram.print();
 		
 		/*
