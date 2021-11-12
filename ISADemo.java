2964e3c80a59cfc67bfb3658c6a9470b6f84e9e6
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-11-12 10:02:36 +0530

    Add files via upload

---
+++ ISADemo.java
@@ -0,0 +1,16 @@
+package com.brainmentors.java.oops;
+
+class Account {
+	
+}
+
+public class ISADemo {
+	public static void main(String[] args) {
+		
+//		S : Single Responsibility Principle
+		
+		
+		
+	}
+
+}
